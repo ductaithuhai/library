ee68c4c7aff3c57f6d09f37574bf120140a69182
ductaithuhai <nductai94@gmail.com>
2023-11-27 23:48:15 +0700

    2nd

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,13 +8,18 @@ function Book(title, author, pages, status) {
 };
 
 var addButton = document.getElementById("add");
-
+var inputField = document.getElementsByClassName('inputfield');
+var finishInput = document.getElementsByClassName('done');
+console.log(finishInput);
 // function addBookToLibrary() {
 //   newBook = new Book('Tai', 'Tai', '460', 'readed');
 // };
 
 addButton.addEventListener("click", function () {
+  inputField[0].style.display = 'flex';
+});
+
+finishInput[0].addEventListener("click", function () {
   let newBook = new Book('Tai', 'Tai', '460', 'readed');
-  alert("Button clicked!");
-  console.log(newBook);
+  inputField[0].style.display = 'none';
 });
